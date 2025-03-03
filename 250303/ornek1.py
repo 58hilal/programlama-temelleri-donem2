3edf61c97215873813ab5bcd850c894cd6941e5b
58hilal <hilalozbek588@gmail.com>
2025-03-03 11:06:34 +0300

    Add files via upload

---
+++ 250303/ornek1.py
@@ -0,0 +1,7 @@
+bagaj=int(input("toplam bagaj miktarını giriniz"))
+if  bagaj<=20
+   print("herhangi bir ucret odemeniz gerekmiyor")
+else:
+    fark = bagaj - 20
+    print("fazla bagaj için "fark * 10 "tl odemelisiniz")
+print("iyi yolculuklar")
