45d10a7e8cd8d0dfef38df7072ee9f89ded05707
58hilal <hilalozbek588@gmail.com>
2025-03-10 12:08:58 +0300

    Add files via upload

---
+++ 250310/ornek2.py
@@ -0,0 +1,6 @@
+k_ad=input("kullanıcı adını girin:")
+sifre=input("sifreyi girin:")
+if(k_ad=="türkiye" and sifre=="1923"):
+    print("giriş başarılı")
+else:
+    print("giriş başarısız")
---
+++ 250310/ornek3.py
@@ -0,0 +1,5 @@
+sayi=int(input("bir sayı girin"))
+if sayi % 5==0 and sayi % 3==0:
+    print("15 e tam bolunur")
+else:
+    print("15 e tam bolunmez")
